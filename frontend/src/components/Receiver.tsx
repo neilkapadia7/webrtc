450a0884cbcd66c6f0f034baa2c2165de4174492
neilkapadia7 <neilkapadia7@gmail.com>
2024-05-24 21:35:55 +0530

    frontend P-to-P Code with WebRTC

--- frontend/src/components/Receiver.tsx
+++ frontend/src/components/Receiver.tsx
@@ -1,8 +1,10 @@
-import { useEffect } from "react"
+import { useEffect, useRef, useState } from "react"
 
 
 export const Receiver = () => {
-    
+    const [recieverSocket, setSocket] = useState<WebSocket | null>(null);
+    const videoRef = useRef<HTMLVideoElement>(null);
+
     useEffect(() => {
         const socket = new WebSocket('ws://localhost:8080');
         socket.onopen = () => {
@@ -10,8 +12,27 @@ export const Receiver = () => {
                 type: 'receiver'
             }));
         }
+        setSocket(socket);
         startReceiving(socket);
     }, []);
+
+
+    // recieverSocket.onmessage = (event) => {
+    //     const message = JSON.parse(event.data);
+    //     if (message.type === 'createOffer') {
+    //         pc.setRemoteDescription(message.sdp).then(() => {
+    //             pc.createAnswer().then((answer) => {
+    //                 pc.setLocalDescription(answer);
+    //                 socket.send(JSON.stringify({
+    //                     type: 'createAnswer',
+    //                     sdp: answer
+    //                 }));
+    //             });
+    //         });
+    //     } else if (message.type === 'iceCandidate') {
+    //         pc.addIceCandidate(message.candidate);
+    //     }
+    // }
 
     function startReceiving(socket: WebSocket) {
         const video = document.createElement('video');
@@ -19,8 +40,12 @@ export const Receiver = () => {
 
         const pc = new RTCPeerConnection();
         pc.ontrack = (event) => {
-            console.log(event);
+            console.log("TRACK ADDED -> ", event);
+            if(videoRef.current) {
+                videoRef.current.srcObject = new MediaStream([event.track]);
+            }
             video.srcObject = new MediaStream([event.track]);
+            video.muted = true;
             video.play();
         }
 
@@ -43,6 +68,6 @@ export const Receiver = () => {
     }
 
     return <div>
-        
+        <video ref={videoRef}></video>
     </div>
 }
